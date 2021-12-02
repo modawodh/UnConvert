e6c91fa3677ac285102cfa73c5c0008b22266cba
mo dawod <modawodh@gmail.com>
2021-12-02 15:43:21 +0430

    complete layout

--- app/src/main/java/com/modawod/unconvert/MainActivity.java
+++ app/src/main/java/com/modawod/unconvert/MainActivity.java
@@ -3,12 +3,21 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
     }
 }
