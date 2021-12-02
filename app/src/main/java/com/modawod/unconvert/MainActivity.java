22db2f7e04dc402f43a1d16e3b536bb5eb679b37
mo dawod <modawodh@gmail.com>
2021-12-02 16:18:33 +0430

    add image

--- app/src/main/java/com/modawod/unconvert/MainActivity.java
+++ app/src/main/java/com/modawod/unconvert/MainActivity.java
@@ -3,9 +3,11 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
  public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -19,5 +21,15 @@ import android.widget.TextView;
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this , "Hi click listener worked", Toast.LENGTH_SHORT).show();
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.205 * kg;
+                textView.setText("The corresponding value in pounds is: " + pound);
+            }
+        });
     }
 }
